d32d61fd210d5897d6f488100e637e99b03ce969
Arya Yadav <aryayadav3497@gmail.com>
2023-06-18 00:43:58 +0530

    feat: added a panel with the name selected shape

--- shapeCreator.js
+++ shapeCreator.js
@@ -7,5 +7,11 @@ dialog.orientation = "column";
 dialog.preferredSize.width = 300;
 dialog.preferredSize.height = 200;
 
+// Add a panel for shape selection with increased size
+var shapePanel = dialog.add("panel", undefined, "Select Shape");
+shapePanel.alignChildren = "left";
+shapePanel.preferredSize.width = 280;
+shapePanel.preferredSize.height = 80;
+
 // Show the dialog box
 dialog.show();
